bc113eb70639258c0e480cb9f132bf6dc5496e7a
ismailofuz <sirojiddin.ismoilov7181@gmail.com>
2022-09-06 10:51:01 +0500

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('API')
+  .setDescription('Class manager API')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
